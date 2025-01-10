892a5190a725ec84d7ca9014c57453158ac166d6
Bluskyfishing <blusky445@gmail.com>
2025-01-10 18:53:09 +0100

    gitignore test

--- dockerDB/katsuyouMakeUsers.sql
+++ dockerDB/katsuyouMakeUsers.sql
@@ -1,5 +1,5 @@
-CREATE USER IF NOT EXISTS 'root'@'%' IDENTIFIED BY 'rootPassword'; --Replace with actual passwords for db.
-CREATE USER IF NOT EXISTS 'mysql'@'%' IDENTIFIED BY 'mysqlPassword';
+CREATE USER IF NOT EXISTS 'root'@'%' IDENTIFIED BY 'uNLNpNTwmHvDT6kXySMR';
+CREATE USER IF NOT EXISTS 'mysql'@'%' IDENTIFIED BY '9L8SfsbXjWwfkcH7ub6P';
 
 GRANT ALL PRIVILEGES ON JMDict.* TO 'root'@'%';
 GRANT ALL PRIVILEGES ON JMDict.* TO 'mysql'@'%';
